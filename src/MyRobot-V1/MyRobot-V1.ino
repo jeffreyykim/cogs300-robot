e132800c3aec1d6030e437ecd476f24f454cd154
Jeffrey Kim <110882794+jeffreyykim@users.noreply.github.com>
2026-01-26 16:08:54 -0800

    - Update loop() to test all turning/movement functions

--- src/MyRobot-V1/MyRobot-V1.ino
+++ src/MyRobot-V1/MyRobot-V1.ino
@@ -26,23 +26,63 @@ void setup() {
 }
 
 void loop() {
-    // Drive forward for 1 second
+    // Drive forward for 2 seconds
     logInfo(Serial, "Moving forward");
     forward(200);
-    delay(1000);
+    delay(2000);
 
     // Stop for 1 second
     logInfo(Serial, "Stopping");
     stop();
     delay(1000);
 
-    // Drive backward for 1 second
+    // Drive backward for 2 seconds
     logInfo(Serial, "Moving backward");
     backward(200);
-    delay(1000);
+    delay(2000);
 
     // Stop for 1 second
     logInfo(Serial, "Stopping");
     stop();
     delay(1000);
+
+    // Turn left for 2 seconds
+    logInfo(Serial, "Turning left");
+    turnLeft(150, 50);
+    delay(2000);
+
+    // Stop for 1 second
+    logInfo(Serial, "Stopping");
+    stop();
+    delay(1000);
+
+    // Turn right for 2 seconds
+    logInfo(Serial, "Turning right");
+    turnRight(150, 50);
+    delay(2000);
+
+    // Stop for 1 second
+    logInfo(Serial, "Stopping");
+    stop();
+    delay(1000);
+
+    // Spin clockwise for 2 seconds
+    logInfo(Serial, "Spinning clockwise");
+    turnInPlace(180, true);
+    delay(2000);
+
+    // Stop for 1 second
+    logInfo(Serial, "Stopping");
+    stop();
+    delay(1000);
+
+    // Spin counter-clockwise for 2 seconds
+    logInfo(Serial, "Spinning counter-clockwise");
+    turnInPlace(180, false);
+    delay(2000);
+
+    // Stop for 2 seconds before repeating
+    logInfo(Serial, "Stopping");
+    stop();
+    delay(2000);
 }
